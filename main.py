53787f25226bc365948c95faca66b96c61c80d8d
migvill5 <mvillacis.mhva@gmail.com>
2020-06-02 09:36:36 -0500

    Main window tkinter and ui folder

---
+++ main.py
@@ -0,0 +1,7 @@
+from uis.general.ui import MainWindow
+
+
+window = MainWindow()
+window.master.title("migvil5 some projects")
+window.master.geometry('250x500')
+window.master.mainloop()
---
+++ uis/general/ui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class MainWindow(ttk.Frame):
+
+    def __ini__(self, master=None):
+        super().__init__(master)
+        self.pack()
